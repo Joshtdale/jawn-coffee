da676e1e83d80074dc2883bc4114c11f02069212
Joshtdale <joshuadale65@gmail.com>
2023-03-06 21:42:32 +0000

    bg rough

--- components/Home.js
+++ components/Home.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function HomeCard(props) {
   return (
-    <div id={props.id} className='homeCard'>{props.id}</div>
+    <div id={props.id} className='homeCard'>
+      <div className='jumbotron'></div>
+    </div>
   )
 }
 
