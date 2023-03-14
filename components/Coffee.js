c1d15e7beef2e3f62c3d0b7f2d42e9e3a679e203
Joshtdale <joshuadale65@gmail.com>
2023-03-14 21:35:51 +0000

    coffee gif added

--- components/Coffee.js
+++ components/Coffee.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Coffee() {
     return (
-        <div id='coffee'>Coffee</div>
+        <div className='' id='coffee'>
+            <img
+                src='https://cdn.shopify.com/s/files/1/0277/2618/4517/collections/Webp.net-gifmaker-2_960x960.gif?v=1648328799'
+                className=''
+                alt="Coffee"
+                width='auto'
+                height='auto'
+            />
+        </div>
     )
 }
 
