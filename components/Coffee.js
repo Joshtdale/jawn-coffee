5039950beda1bde4a6132dea209d5589a6cf83cf
Joshtdale <joshuadale65@gmail.com>
2023-03-16 23:41:30 +0000

    coffee section rough outline

--- components/Coffee.js
+++ components/Coffee.js
@@ -3,14 +3,23 @@ import Image from 'next/image'
 
 function Coffee() {
     return (
-        <div className='' id='coffee'>
-            <img
-                src='https://cdn.shopify.com/s/files/1/0277/2618/4517/collections/Webp.net-gifmaker-2_960x960.gif?v=1648328799'
-                className=''
-                alt="Coffee"
-                width='auto'
-                height='auto'
-            />
+        <div className='row d-flex justify-content-center align-items-center coffeeSection' id='coffee'>
+            <div className='col-4 d-flex justify-content-center'>
+                <img
+                    src='https://cdn.shopify.com/s/files/1/0277/2618/4517/collections/Webp.net-gifmaker-2_960x960.gif?v=1648328799'
+                    className='coffeePreview'
+                    alt="Coffee"
+                    width='auto'
+                    height='auto'
+                />
+            </div>
+            <div className="col-4">
+                <h1>Something about coffee</h1>
+                <p>Stuff about the coffee coming from proud hound Stuff about the coffee coming from proud hound 
+                    Stuff about the coffee coming from proud hound Stuff about the coffee coming from proud hound 
+                    Stuff about the coffee coming from proud hound Stuff about the coffee coming from proud hound 
+                    Stuff about the coffee coming from proud hound Stuff about the coffee coming from proud hound </p>
+            </div>
         </div>
     )
 }
